8f0081aaf6f55562e9bb15beac5138fbf9c44d51
Emanuel Allaria <allariaemanuel@gmail.com>
2023-03-07 22:29:02 -0300

    1.0

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const video = document.getElementById('camara');
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const resultado = document.getElementById('resultado');
 
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then((stream) => {
-    video.srcObject = stream;
-    video.play();
-    requestAnimationFrame(escanearCodigoQR);
-  });
+navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}})
+    .then(stream => {
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.setAttribute('playsinline', '');
+        video.play();
 
-function escanearCodigoQR() {
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const codigoQR = jsQR(imageData.data, imageData.width, imageData.height);
-    if (codigoQR) {
-      console.log(codigoQR.data);
-    }
-  }
-  requestAnimationFrame(escanearCodigoQR);
-}
+        requestAnimationFrame(tick);
+
+        function tick() {
+            if (video.readyState === video.HAVE_ENOUGH_DATA) {
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+                const imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+                const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                    inversionAttempts: 'dontInvert'
+                });
+
+                if (code) {
+                    resultado.textContent = code.data;
+                }
+            }
+
+            requestAnimationFrame(tick);
+        }
+    })
+    .catch(error => console.error(error));
