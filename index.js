97a09890837093c9da517ed528770488e24e9a5a
Emanuel Allaria <allariaemanuel@gmail.com>
2023-03-07 22:44:30 -0300

    1.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const resultado = document.getElementById('resultado a');
+const resultado = document.getElementById('resultado');
 
 navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}})
     .then(stream => {
