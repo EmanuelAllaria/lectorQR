264bfc1d7dc678b01fc363d2ef422050f7061d99
Emanuel Allaria <allariaemanuel@gmail.com>
2023-03-07 22:23:15 -0300

    1.2

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const canvas = document.getElementById('canvas');
-const resultado = document.getElementById('resultado');
+const video = document.getElementById('camara');
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
 
-navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}})
-    .then(stream => {
-        const video = document.createElement('video');
-        video.srcObject = stream;
-        video.setAttribute('playsinline', '');
-        video.play();
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    video.srcObject = stream;
+    video.play();
+    requestAnimationFrame(escanearCodigoQR);
+  });
 
-        requestAnimationFrame(tick);
-
-        function tick() {
-            if (video.readyState === video.HAVE_ENOUGH_DATA) {
-                canvas.width = "100%";
-                canvas.height = "100%";
-                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-                const imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-                const code = jsQR(imageData.data, imageData.width, imageData.height, {
-                    inversionAttempts: 'dontInvert'
-                });
-
-                if (code) {
-                    resultado.textContent = code.data;
-                }
-            }
-
-            requestAnimationFrame(tick);
-        }
-    })
-    .catch(error => console.error(error));
+function escanearCodigoQR() {
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const codigoQR = jsQR(imageData.data, imageData.width, imageData.height);
+    if (codigoQR) {
+      console.log(codigoQR.data);
+    }
+  }
+  requestAnimationFrame(escanearCodigoQR);
+}
